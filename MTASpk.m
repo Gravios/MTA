d19708a074076aa597d77d745c635a772359595c
Gravios <justingraboski@gmail.com>
2014-01-30 11:12:21 +0100

    Comments in MTASpk

--- MTASpk.m
+++ MTASpk.m
@@ -1,15 +1,59 @@
 classdef MTASpk < hgsetget
+% MTASpk(varargin)
+% Data structure to organize timing and features of neuron action potentials.
+%
+%   Note: The properties (clu,res,fet,spk) should all have the same length
+%         along the first dimension.
+%   Note: MTASpk does not have a sync property. At the moment, anytime
+%         spiking information is needed, it should be generated from the
+%         orginal files and synced to the current MTATrial or MTASession.
+%   Note: MTASpk.create only loads clu res and map at the moment.
+%   Note: MTASpk.create is only compatible with the Neuralynx system.
+%
+%   varargin:
+%     [res,clu,map,sampleRate,fet,spk,type] 
+%
+%     res:    columnArray(spkIndex,Timestamp), spike timing index in the 
+%                                              specified sampleRate 
+%
+%     clu:    columnArray(spkIndex,ClusterId), spike cluster identity for 
+%                                              each spike
+%
+%     map:    matrix     (ClusterId,ElectrodeGroup,ElectrodeClusterId)
+%                                              Cluster/electrode mapping
+%
+%     sampleRate: double, the sampling rate to which the spike indicies
+%                         correspond 
+%
+%     fet:    matrix     (spkIndex,Feature),   First couple of spike waveform
+%                                              principle components for each
+%                                              electrode
+%
+%     spk:    matrix     (spkIndex,spkWaveform), Filtered spike waveforms
+%
+%     type:   string, Data type
+
     properties(Transient=true)
-        
+        %clu - columnArray: spike cluster identity for each spike
         clu
         
+        %res - columnArray: spike timing index in the specified sampleRate 
         res
+        
+        %fet - matrix: spike waveform features
+        fet
 
+        %spk - matrix: Filtered spike waveforms
+        spk
+        
+        %sampleRate - double: the sampling rate to which the spike indicies correspond
         sampleRate    
     end
     properties
+        %map - matrix: Cluster/electrode mapping
         map
-
+     
+        %type - string: Data type
         type
     end
     methods
@@ -18,13 +62,39 @@ classdef MTASpk < hgsetget
         %s.spk(s.stc{'rear'},
         
         function Spk = MTASpk(varargin)
-            [Spk.res,Spk.clu,Spk.map,Spk.sampleRate,Spk.type] = ...
+        % Spk = MTASpk(varargin)
+        % The general constuctor for MTASpk 
+        % Normal usage is to create an Spk object with all values empty 
+        % and then use the create function to populate the properties
+        
+            [Spk.res,Spk.clu,Spk.map,Spk.sampleRate,Spk.fet,Spk.spk,Spk.type] = ...
                 DefaultArgs(varargin,{[],[],[],[],'TimePoints'});            
         end
         function Spk = create(Spk,Session,varargin)
+        % Spk = create(Spk,Session,varargin)
+        % Function used to load spiking data from {clu,res,fet,spk} file
+        % and synchronize with the Session
+        %   
+        %   varargin:
+        %     [sampleRate,states,units,loadField] 
+        %
+        %     sampleRate: double, the sampleRate in Hertz
+        %
+        %     states:     string, state label or expression to select for a
+        %                         subset of spikes
+        %
+        %     units:       array, Cluster identities to select for a subset
+        %                         of spikes. The default [] will return all
+        %                         Clusters.
+        %
+        %     loadField: cellarry, NOT IMPLEMENTED
+        %
+        % See also MTAStateCollection for more information on selecting
+        % states
+        %
             %% Load and resample Res             
-            [Spk.sampleRate,states,units] = DefaultArgs(varargin,{1,[],[]});
-            
+            [sampleRate,states,units,loadField] = DefaultArgs(varargin,{1,[],[],{}});
+            Spk.sampleRate = sampleRate;
             [Res, Clu, Map] = LoadCluRes(fullfile(Session.spath, Session.name));
             Spk.map = Map;
             Res = Res/Session.sampleRate*Spk.sampleRate;
