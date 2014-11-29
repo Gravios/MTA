26e395cbc24230fe95dab26f2fec59c16ef6f343
Gravios <justingraboski@gmail.com>
2014-11-29 09:32:34 +0000

    corrected pB error

--- utilities/pB.m
+++ utilities/pB.m
@@ -2,5 +2,5 @@ function pB(Trial)
 xyz = Trial.load('xyz');
 figure,hold on
 plot(xyz(:,Trial.trackingMarker,3));
-Lines((s.xyz.sync.data(:,1)-s.xyz.sync.data(1))*s.xyz.sampleRate,[],'r')
-Lines((s.xyz.sync.data(:,2)-s.xyz.sync.data(1))*s.xyz.sampleRate,[],'k')
+Lines((xyz.sync.data(:,1)-xyz.sync.data(1))*xyz.sampleRate,[],'r');
+Lines((xyz.sync.data(:,2)-xyz.sync.data(1))*xyz.sampleRate,[],'k');
