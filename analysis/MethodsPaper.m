e7664fb2d1c5bf67db6b9085b8339a7c8b4163b7
Gravios <justingraboski@gmail.com>
2015-01-16 09:06:24 +0100

    methods stuff marker error and rhm ncp comp

--- analysis/MethodsPaper.m
+++ analysis/MethodsPaper.m
@@ -1,5 +1,51 @@
 
 % figure 1
+Trial = MTASession('jg05-20120317');
+xyz = Trial.load('xyz');
+ang = Trial.ang.copy;
+ang.create(Trial,xyz);
+hfig = figure(838883);
+N = histc(ang(Trial.stc{'a'},5,6,3),31:.1:37);
+bar(31:.1:37,N,'histc')
+
+
+vel = xyz.vel([5:9]);
+vel.data(vel<0.01) = 0.01;
+vel.data = log10(vel.data);
+
+m = {'head_back','head_right'};
+vbins = -.5:.05:2;
+[~,mb(:,1)] = histc(vel(:,m{1}),vbins);
+[~,mb(:,2)] = histc(vel(:,m{2}),vbins);
+
+mb = MTADxyz('data',mb,'sampleRate',xyz.sampleRate);
+
+ind = resample(Trial.stc{'t'}.cast('TimeSeries'),xyz)&nniz(mb);
+%ind = nniz(mb);
+%ind = ':';
+
+
+A = accumarray(mb(ind,:),ang(ind,m{1},m{2},3),repmat(numel(vbins),[1,2]),@mean);
+B = accumarray(mb(ind,:),ang(ind,m{1},m{2},3),repmat(numel(vbins),[1,2]),@std);
+S = accumarray(mb(ind,:),ind(ind),repmat(numel(vbins),[1,size(mb,2)]),@sum);
+A(S<100)=nan;
+B(S<100)=nan;
+
+% mean marker distance vs speed
+figure,
+subplot(121)
+imagescnan({vbins,vbins,A'},prctile(A(nniz(A(:))),[5,95]),[],true,[0,0,0]),axis xy,
+subplot(122)
+imagescnan({vbins,vbins,B'},[0,2],[],true,[0,0,0]),axis xy
+
+figure,
+hist(ang(Trial.stc{'a'},'head_back','head_front',3),20:.02:37)
+figure,
+hist(ang(Trial.stc{'a'},'head_right','head_front',3),20:.02:37)
+figure,
+hist(ang(Trial.stc{'a'},'head_right','head_left',3),20:.02:37)
+figure,
+hist(ang(Trial.stc{'a'},'head_back','head_right',3),10:.02:37)
 
 
 % subplot f
@@ -7,30 +53,33 @@
 
 Trial = MTASession('Ed10-20140812');
 
+
 xyz = Trial.load('xyz');
 
-
-
-
-lfp = Trial.lfp.copy;
-lfp.create(Trial,66);
-lfp.resample(xyz);
-
-
-
-specParms = struct('nFFT',2^11,...
-                   'Fs',lfp.sampleRate,...
-                   'WinLength',2^10,...
-                   'nOverlap',2^10*.875,...
-                   'FreqRange',[1,15]);
-
-
-lfp.data = cat(2,lfp.data,xyz(:,1,3));
-lfp.data = cat(2,lfp.data,xyz(:,3,3));
+% lfp = Trial.lfp.copy;
+% lfp.create(Trial,66);
+% lfp.resample(xyz);
+% 
+% 
+% 
+% specParms = struct('nFFT',2^11,...
+%                    'Fs',lfp.sampleRate,...
+%                    'WinLength',2^10,...
+%                    'nOverlap',2^10*.875,...
+%                    'FreqRange',[1,15]);
+% 
+% 
+% lfp.data = cat(2,lfp.data,xyz(:,1,3));
+% lfp.data = cat(2,lfp.data,xyz(:,3,3));
 
 xyz.filter(gtwin(.5,xyz.sampleRate));
 ang = Trial.ang.copy;
 ang.create(Trial,xyz);
+
+figure,
+plot(sq(ButFilter(xyz(:,[1:3],3)));
+
+figure,plot(ang(:,1,3,3));
 
 
 rhm = fet_rhm(Trial);
@@ -40,5 +89,21 @@ lfp.data = cat(2,lfp.data,rhm);
 
 
 figure,imagesc(ts,fs,ys(:,:,1,2)'),axis xy
+
+
+%% Figure 5 RHM (rythmic head motion) feature versus NCP (nasal cavity pressure)
+Trial = MTATrial(Ed10-20140812);
+
+%generate features
+rhm = fet_rhm(Trial,[],'wcsd');
+ncp = fet_ncp(Trial,[],'wcsd');
+%plot features with linked axes
+
+figure,
+sp(1) = subplot(211);
+imagesc(ts,fs,log10(rhm.data)),axis xy,caxis([-6,-4])
+sp(2) = subplot(212);
+imagesc(ts,fs,log10(ncp.data)),axis xy,caxis([-6,-4])
+linkaxes(sp,'xy');
 
 
