6eff49f2bc579a7b6f8645183729946c552e4b29
Gravios <justingraboski@gmail.com>
2015-02-09 14:01:50 +0100

    updated MTASession load to search for relevat superclasses rather an immediate class

--- utilities/stc2mat.m
+++ utilities/stc2mat.m
@@ -1,4 +1,4 @@
-function [smat,keys,labels] = stc2mat(Stc,Data,varargin)
+function [smat,labels,keys] = stc2mat(Stc,Data,varargin)
 %function [smat,keys,labels] = stc2mat(Stc)
 % assumes no heirarchical relationships between states
 % each is mutually exclusive from all others
@@ -10,13 +10,13 @@ end
 
 nsts = numel(states);
 smat = zeros([Data.size(1),nsts]);
-keys = '';
+keys = {};
 labels = {};
 for i = 1:nsts
     tper = resample(Stc{states{i}}.cast('TimeSeries'),Data);
     smat(tper==1,i) = i;
     if nargout>1,
-        keys(i) = tper.key;
+        keys{i} = tper.key;
         labels{i} = tper.label;
     end
 end
