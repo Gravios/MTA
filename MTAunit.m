0f875d4ea2fe8fbf7509a2c6e5f0224306ab7ef3
Gravios <justingraboski@gmail.com>
2015-02-09 13:27:29 +0100

    a headache saved for later

---
+++ MTAunit.m
@@ -0,0 +1,23 @@
+classdef MTAunit
+
+    properties
+        cluster
+        electrode
+        elcluster
+        stcStats
+        
+        
+    end
+
+    methods(access=public)
+        
+        function MTAunit(cluster,electrode,elcluster)
+
+        end
+    
+    end
+
+    
+
+
+end
