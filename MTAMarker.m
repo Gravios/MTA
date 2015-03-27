47e7bc4efc73cb663bf642ca27d529c72739d923
Justin Graboski <justingraboski@gmail.com>
2015-03-27 15:18:44 +0100

    MTAMarker update added function short returns shortened name of marker

--- MTAMarker.m
+++ MTAMarker.m
@@ -1,4 +1,14 @@
 classdef MTAMarker
+% MTAMarker
+% Class which holds general information and graphical properties of
+% the markers.
+%
+% The naming convention of markers is restricted by the C3D format
+% to a set length of ##. The name of a marker should be a two word
+% or a double acronym separated by an underscore. Choose words
+% whos initials are unique (necessary for clarity when the short
+% function is called).
+%
     properties (SetAccess = public)
         name
         color
@@ -10,6 +20,15 @@ classdef MTAMarker
                 Marker.color = str2num(color);
             end
         end
+        
+        function strout = short(Marker)
+            t = 'head_front';
+            p = '(^.)|(_.)';
+            ts = strjoin(regexpi(t,p,'match'),'');
+            strout = upper(ts([1:2:numel(ts)]));
+        end
+        
     end
 end
+
 
