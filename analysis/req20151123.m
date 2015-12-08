24f29258bdb875e801ca4d5fba3e7be8906edf44
Justin Graboski <justingraboski@gmail.com>
2015-12-08 17:27:31 +0100

    hmm not sure what I did

--- analysis/req20151123.m
+++ analysis/req20151123.m
@@ -7,10 +7,20 @@ seslist = {{'er01-20110719','hand_labeled_rev1'},...
            {'jg05-20120317','hand_labeled_rev2'},...
            {'Ed01-20140707','hand_labeled_rev1'},...
            {'Ed03-20140624','hand_labeled_rev1'}};
+
+seslist = {...
+           {'jg05-20120309','auto_wbhr'},...
+           {'jg05-20120310','auto_wbhr'},...
+           {'jg05-20120311','auto_wbhr'},...
+           {'jg05-20120315','auto_wbhr'},...           
+           {'jg05-20120317','auto_wbhr'},...
+           {'jg05-20120324','auto_wbhr'},...    
+           {'jg05-20120325','auto_wbhr'}};
+
 nses = numel(seslist);
 
-cs = 'rbgc';
-Trial = {}; Stc = {}; xyz = {};
+cs = jet(numel(seslist));
+Trial = {}; Stc = {}; xyz = {}; fvel = {};
 for i = 1:nses
     % Load Trials into cell arrray
     Trial{i} = MTATrial(seslist{i}{1});   
@@ -22,32 +32,42 @@ for i = 1:nses
     % pass filter with upper limit at 2.4Hz
     fvel {i} = xyz{i}.vel([],[1,2]).filter('ButFilter',3,2.4,'low');
     % Transform lower spine marker speed to a log scale
-    fvel{i}(fvel{i}<0.01) = .01;
+    fvel {i}.data(fvel{i}.data<0.01) = .01;
     fvel {i}.data = log10(fvel{i}.data);
 end
 
 
+%%Start here
+
 % Overlayed histograms of lower spine height
-eds = linspace(0,80,100);
-figure,hold on
+eds = linspace(-10,80,100);
+eds = linspace(40,130,100);
+eds = linspace(50,170,100);
+eds = linspace(50,200,100);
+figure,hold on,hout = {};
+m = 1;
 for i = 1:nses,
-    ind = Stc{i}{'a'};
-    hn = bar(eds,histc(xyz{i}(ind,1,3),eds)./sum(diff(ind.data,1,2)),'histc');
-    hn.FaceColor = cs(i); hn.FaceAlpha = .4; hn.EdgeAlpha = 0; % Modifyle
-                                                               % bar properties
+% $$$     ind = Stc{i}{'a'};
+    ind = fvel{i}(:,1)>0.5&fvel{i}(:,7)>0.5;
+    hout{i} = xyz{i}(ind,m,3);
+    hn = bar(eds,histc(hout{i},eds)./sum(ind),'histc');%diff(ind.data,1,2)
+    hn.FaceColor = cs(i,:); hn.FaceAlpha = .4; hn.EdgeAlpha = 0; % Modifyle
+                                                                 % bar properties
+    hout{i}(hout{i}>80|hout{i}<0) = [];
 end
 legend(cellfun(@subsref,Trial,repmat({substruct('.','name')},size(Trial)),'uniformoutput',false))
     
 
 % Vertically arrange individual histograms of lower spine hight
 figure,hold on
-eds = linspace(0,80,100);
+eds = linspace(-10,80,100);
 for i = 1:nses,
     subplot(nses,1,i);
     ind = Stc{i}{'a'};
     bar(eds,histc(xyz{i}(ind,1,3),eds)./sum(diff(ind.data,1,2)),'histc');
     title(Trial{i}.filebase);
 end
+
 
 
 % JPDF of lower spine height vs lower spine speed over all hand
