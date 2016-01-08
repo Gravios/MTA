ad9b57fb46587fd5e85b7e72c3952c8d194d6dbf
Justin Graboski <justingraboski@gmail.com>
2016-01-08 20:09:49 +0100

    added conversion function to assign standard keys to MTADepochs of hand labeled State Colletions

---
+++ analysis/convert_stc_Ed_to_jg.m
@@ -0,0 +1,20 @@
+function convert_stc_Ed_to_jg(Trial)
+
+if ischar(Trial),
+    Trial = MTATrial(Trial);
+elseif iscell(Trial),
+    Trial = MTATrial(Trial{:});
+end
+
+Trial.load('stc','manual1');
+
+Trial.stc.states{Trial.stc.gsi('pause')}.key = 'p';
+Trial.stc.states{ Trial.stc.gsi('walk')}.key = 'w';
+Trial.stc.states{ Trial.stc.gsi('rear')}.key = 'r';
+Trial.stc.states{ Trial.stc.gsi('turn')}.key = 'n';
+Trial.stc.states{Trial.stc.gsi('shake')}.key = 'k';
+Trial.stc.states{  Trial.stc.gsi('sit')}.key = 's';
+Trial.stc.states{Trial.stc.gsi('groom')}.key = 'm';
+
+Trial.stc.updateMode('hand_labeled_rev1');
+Trial.stc.save(1);
