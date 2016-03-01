456667587c6199029fed9749348c798ca7cfa032
Justin Graboski <justingraboski@gmail.com>
2016-03-01 14:53:40 +0100

    generate_enf ready for testing

--- Vicon/generate_enf.m
+++ Vicon/generate_enf.m
@@ -1,22 +1,15 @@
 function generate_enf(path,varargin)
-if isemtpy(varargin)
-    pattern = 'Trial.*\.Trial\d{1,5}\.enf';
-else
-    pattern = varargin{1}    
-end
-path = 
 
-files = dir(path)
-flist = {files(~cellfun(@isempty,regexp({files.name},pattern))).name};
+overwrite = false; if ~isempty(varargin) overwrite = true; end
 
+path = '/storage/gravio/data/raw/vicon/vicon/Top Level 2/jg03/jg03-20110501/';
 
-
-enf_content = [...
+enfContent = [...
 '[Node Information]\n'...
 'TYPE=TRIAL\n'...
 'Reference=\n'...
 'PARENT=Session 1\n'...
-'NAME=Trial001_1\n'...
+'NAME=%s\n'...
 'HASCHILD=FALSE\n'...
 'SORTBY=1\n'...
 '[TRIAL_INFO]\n'...
@@ -25,3 +18,32 @@ enf_content = [...
 '[LOCK_INFO]\n'...
 'CURR_USER=\n'...
 ];
+
+files = dir(path)
+flist = {files(~cellfun(@isempty,regexp({files.name},'.*\.Trial\d{1,5}\.enf'))).name};
+clist = {files(~cellfun(@isempty,regexp({files.name},'.*\.c3d'))).name};
+
+mtr = regexp(flist,'.*\.Trial(\d{1,5})\.enf','tokens');
+for m = 1:numel(mtr), mtr{m} = mtr{m}{1}{1}; end
+mtr = max(cell2mat(mtr));
+
+for c = 1:numel(clist),    
+    existingEnf = regexp(flist,[clist{c}(1:end-4),'\.Trial\d{1,5}\.enf'],...
+                          'match');
+    enfInd = find(~cellfun(@isempty,existingEnf),1,'first');
+    if isemtpy(enfInd) enfInd = 1; end
+    
+    if isemtpy(existingEnf{enfInd}{1}),
+        mtr = mtr+1;        
+        enfFileName = [clist{c}(1:end-4) 'Trial' num2str(mtr) '.enf'];
+    elseif overwrite                       
+        enfFileName = existingEnf{enfInd}{1};
+    else
+        continue
+    end
+    
+    fid = fopen(fullfile(path,enfFileName),'w');
+    fprintf(fid,enfContent,clist{c}(1:end-4));
+    fclose(fid);
+end
+
--- analysis/req20160224.m
+++ analysis/req20160224.m
@@ -1,20 +1,39 @@
 
-
+train = false;
+test = ~train;
 states = {'walk','rear','turn','pause','groom','sit'};
+target = 'sit';
 sampleRate = 12;
+gStates = states;
 
-%Train Parm
-Trial = MTATrial.validate('jg05-20120317');
-Trial.load('stc','hand_labeled_rev3_jg');
-RefTrial = [];
-rMean = []; rStd = [];
 
-%Test Parm
-Trial = MTATrial.validate('Ed03-20140625');
-stc = Trial.load('stc','hand_labeled_rev1_Ed');
-RefTrial = MTATrial.validate('jg05-20120317');
-RefTrial.load('stc','hand_labeled_rev3_jg');
-[~,rMean,rStd] = unity(fet_all(RefTrial,sampleRate,[]));
+if train
+    %Train Parm
+    Trial = MTATrial.validate('jg05-20120317');
+    Trial.load('stc','hand_labeled_rev3_jg');
+    stc = Trial.stc.copy;
+    RefTrial = [];
+    rMean = []; rStd = [];
+end
+
+if test
+    %Test Parm
+    %Trial = MTATrial.validate('Ed03-20140625');
+    Trial = MTATrial.validate({'Ed05-20140529','all','ont'});    
+    Trial.load('stc','hand_labeled_rev1_Ed');
+    stc = Trial.stc.copy;
+    RefTrial = MTATrial.validate('jg05-20120317');
+    RefTrial.load('stc','hand_labeled_rev3_jg');
+    rfet = fet_all(RefTrial,sampleRate,[]);
+    rfet.data = [rfet.data,rfet.data.^2];
+    rafet = rfet.copy;
+    for sh = 1:rfet.size(2)-1;
+        rfet.data = [rfet.data,circshift(rafet.data',-sh)'.*rafet.data];
+    end
+    [~,rMean,rStd] = unity(rfet);
+    clear('rfet')
+end
+
 
 fet = fet_all(Trial,sampleRate,RefTrial);
 fet.data = [fet.data,fet.data.^2];
@@ -30,63 +49,94 @@ else
 end
 
 
-[tstc,~,tfet] = resample_whole_state_bootstrap_noisy_trim(stc,fet,states);
+if train
+    [tstc,~,tfet] = resample_whole_state_bootstrap_noisy_trim(stc,fet,states);
 
-fetInds = {};
-gStates = states;
+    fetInds = {};
+    gStates = states;
 
-mixy = zeros([1+numel(gStates),tfet.size(2)]);
-for s = 0:numel(gStates),
-    
-    if s==0,
-        tStates = gStates;
-    else
-        tStates = gStates(~ismember(1:numel(gStates),s));
-    end
-
-    sm = stc2mat(tstc,tfet,tStates);
-    [~,smat] = max(sm,[],2);
-    smat(all(sm==0,2)) = 0;
-    
-    vind = smat&nniz(tfet);
-    nind = sum(vind);
-
-    for f = 1:tfet.size(2),
-
-        if numel(tStates)==6,
-            lsp{f} = mat2cell([prctile(tfet(vind,f),[1,99]),2^7],1,[1,1,1]);
+    mixy = zeros([1+numel(gStates),tfet.size(2)]);
+    for s = 0:numel(gStates),
+        
+        if s==0,
+            tStates = gStates;
+        else
+            tStates = gStates(~ismember(1:numel(gStates),s));
         end
-        
-        
-        edx = linspace(lsp{f}{:});
-        edy = .5:numel(tStates)+.5;
-        
-        fx = tfet(vind,f);
-        fy = smat(vind);
 
-        [out,xb,yb,p]=hist2([fx,fy],edx,edy);
-        pxy = out./nind;
-        px = histc(fx,xb); px = px(1:end-1)/nind;
-        py = histc(fy,yb); py = py(1:end-1)/nind;
-        mixy(s+1,f) = nansum(nansum(pxy.*log2(pxy./(px*py'))));
+        sm = stc2mat(tstc,tfet,tStates);
+        [~,smat] = max(sm,[],2);
+        smat(all(sm==0,2)) = 0;
+        
+        vind = smat&nniz(tfet);
+        nind = sum(vind);
+
+        for f = 1:tfet.size(2),
+
+            if numel(tStates)==6,
+                lsp{f} = mat2cell([prctile(tfet(vind,f),[1,99]),2^7],1,[1,1,1]);
+            end
+            
+            
+            edx = linspace(lsp{f}{:});
+            edy = .5:numel(tStates)+.5;
+            
+            fx = tfet(vind,f);
+            fy = smat(vind);
+
+            [out,xb,yb,p]=hist2([fx,fy],edx,edy);
+            pxy = out./nind;
+            px = histc(fx,xb); px = px(1:end-1)/nind;
+            py = histc(fy,yb); py = py(1:end-1)/nind;
+            mixy(s+1,f) = nansum(nansum(pxy.*log2(pxy./(px*py'))));
+        end
+
     end
 
+    figure,
+    for s = 1:numel(gStates)
+        subplot(2,3,s);
+        plot(mixy([1,s+1],:)');
+    end
+
+    sind = 2; % rear
+    dm = (mixy(1,:)-mixy(sind+1,:))';
+    fetInds{end+1} =  find(dm>0.20);
+
+    %state matrix of union between all states minus trarget and target
+    smat = stc2mat(tstc,fet,{[strjoin({states{find(cellfun(@isempty,regexp(states,states{sind})))}},'+'),'&gper'],states{sind}});
+    net = patternnet(200);
+    [net,tr] = train(net,tfet(nniz(tfet),fetInds{end})',smat(nniz(tfet),:)');
+
 end
 
-figure,
-for s = 1:numel(gStates)
-    subplot(2,3,s);
-    plot(mixy([1,s+1],:)');
-end
-
-sind = 2; % rear
-dm = (mixy(1,:)-mixy(sind+1,:))';
-fetInds{end+1} =  find(dm>0.20);
 
 
-smat = [[ones(7500,1);zeros(7500,1);ones(30000,1)],[zeros(7500,1);ones(7500,1);zeros(30000,1)]];
-[net,tr] = train(net,tfet(nniz(tfet),:)',smat(nniz(tfet),:)');
+d_state = net(fet(:,fetInds{end})')';
 
-d_state = net(fet.data')';
+figure,plot(d_state)
+Lines(stc{'r',12}(:),[],'r');
 
-stc = Trial.load('stc','hand_labeled_rev3_jg');
+rper = stc{'r',12};
+rper.cast('TimeSeries');
+rper.resample(fet);
+rper.data(isnan(rper.data)) = 0;
+
+aper = stc{'a',12};
+aper.cast('TimeSeries');
+aper.resample(fet);
+aper.data(isnan(aper.data)) = 0;
+
+dper = stc{'a',12};
+dper.data = ThreshCross(d_state(:,2),0.5,3);
+dper = dper+[-.1,.2];
+dper.cast('TimeSeries');
+dper.resample(fet);
+dper.data(isnan(dper.data)) = 0;
+
+sum(aper.data.*(rper.data&dper.data))./sum(aper.data.*rper.data)
+sum(aper.data.*(rper.data&dper.data))./sum(aper.data.*dper.data)
+
+figure
+hold on,plot(d_state(:,2)) 
+Lines(stc{'r',12}(:),[],'r');
