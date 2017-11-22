0f43bad7c0417700b47c80cf4b8442fa2936f7c1
Gravios <justingraboski@gmail.com>
2017-11-22 09:16:00 +0100

    place field population analysis

--- analysis/MjgER2016/MjgER2016_figure3.m
+++ analysis/MjgER2016/MjgER2016_figure3.m
@@ -21,9 +21,9 @@ statesCcg = {'loc','lloc','hloc','rear',    ...
           'theta-groom-sit'};
 
 overwrite = false;
-testRequested = true;
+testRequested = false;
 
-pfstats = cf(@(t)  compute_pfstats_bs(t),       Trials);
+% pfstats = cf(@(t)  compute_pfstats_bs(t),       Trials);
 % $$$ cf(@(t)  MjgER2016_drzfields(t,true), Trials);
 
 % FOR each Trial -------------------------------------------------------------
@@ -32,11 +32,11 @@ for t = 1:20,
     Trial = Trials{t};
     mkdir(fullfile(FigDir,Trial.filebase));
     stc = Trial.stc.copy();
-    spk = Trial.spk.copy();
-    spk.load_spk(Trial);
+    %spk = Trial.spk.copy();
+    %spk.load_spk(Trial);
     xyz = Trial.load('xyz');
-    pch = fet_HB_pitch(Trial);
-    map_to_reference_session(pch,Trial,pitchReferenceTrial);    
+    %pch = fet_HB_pitch(Trial);
+    %map_to_reference_session(pch,Trial,pitchReferenceTrial);    
     
     pfstats = compute_pfstats_bs(Trial,'overwrite',overwrite);
     units = pfstats.cluMap;
@@ -52,6 +52,15 @@ for t = 1:20,
         defargs = struct2varargin(defargs);        
         pfkbs{sts} = MTAAknnpfs_bs(Trial,defargs{:});      
     end
+
+    unit = 150;
+    s = 1;
+    o = 1;
+    rmap = plot(pfkbs{o},unit);
+    patchInd = pfstats.pfmstats(s,unit==pfstats.cluMap).patchRateInd(1,1,:,:);
+    oPatchRates = rmap(patchInd(1,:),patchInd(2,:));
+    
+    
 
     
     figure();
@@ -72,151 +81,14 @@ for t = 1:20,
     dfst = {'pitch','height','rhm'};
 
 % COMPUTE place fields and subsampled estimate
-    for sts = 1:numel(states),
-        [bhvccg{sts},sper{sts}] = gen_bhv_ccg(Trial,statesCcg{sts},0.5);
-    end
-    sper{end}{1}(sper{end}{1}>(size(xyz,1)./xyz.sampleRate.*1250)) = [];
-    sper{end}{2}(sper{end}{2}>(size(xyz,1)./xyz.sampleRate.*1250)) = [];    
+% $$$ for sts = 1:numel(states),
+% $$$         [bhvccg{sts},sper{sts}] = gen_bhv_ccg(Trial,statesCcg{sts},0.5);
+% $$$     end
+% $$$     sper{end}{1}(sper{end}{1}>(size(xyz,1)./xyz.sampleRate.*1250)) = [];
+% $$$     sper{end}{2}(sper{end}{2}>(size(xyz,1)./xyz.sampleRate.*1250)) = [];    
 
 
-    [accg,tbins] = autoccg(Trial);
+    %[accg,tbins] = autoccg(Trial);
 
-    hfig = figure();
-    hfig.Position = [273, 54, 1269, 681];
-    hfig.PaperPositionMode = 'auto';
-    ny = 12;
-
-    mCom = pfstats.peakPatchCOM;
-    mCom(mCom==0) = nan;
-    mCom = sq(mean(pfstats.peakPatchCOM,2,'omitnan'));
-        
-    for u = 1:numel(units),
-
-        clf();
-        unit = units(u);
-        mpfsRate = max(cell2mat(cf(@(p,u) max(p.maxRate(u)),...
-                                   pfkbs,repmat({unit},[1,numel(bhvccg)]))));
-        if mpfsRate<=0,mpfsRate=1;end % default to 1 if 0
-        mccgRate = max(cell2mat(cf(@(c,u) max(max(c.ccg(:,u,:))),...
-                                   bhvccg,repmat({unit},[1,numel(bhvccg)]))));
-        if mccgRate<=0,mccgRate=1;end % default to 1 if 0
-
-        uRes = spk.res(spk.clu==unit);
-        uSpk = spk.spk(spk.clu==unit,:,:);
-        [~,sInd] = SelectPeriods(uRes,[stc{states{end},1}],'d',1,0);
-        if numel(sInd)<=1, mspkt = zeros([size(uSpk,2),size(uSpk,3)]);
-        else,             mspkt = bsxfun(@plus,sq(mean(uSpk(sInd,:,:)))',fliplr(1000:1000:8000));
-        end
-
-        % row1col1 - auto ccg
-        subplot2(ny,numel(states)+2,1:2,1);
-        bar(tbins,accg(:,unit));axis tight;
-
-        % row1col1 - pitch hl loc distrib
-        subplot2(ny,numel(states)+2,3:4,1);hold('on');
-        ind = [stc{'lloc'}];
-        hax = bar(linspace(-pi/2,pi/2,50),histc(pch(ind,3),linspace(-pi/2,pi/2,50)),'histc');
-        hax.FaceColor = 'c';
-        hax.EdgeColor = 'c';
-        hax.FaceAlpha = 0.5;
-        hax.EdgeAlpha = 0.5;
-        ind = [stc{'hloc'}];
-        hax = bar(linspace(-pi/2,pi/2,50),histc(pch(ind,3),linspace(-pi/2,pi/2,50)),'histc');
-        hax.FaceColor = 'r';
-        hax.EdgeColor = 'r';
-        hax.FaceAlpha = 0.5;
-        hax.EdgeAlpha = 0.5;
-
-        % row1col1 - pitch hl pause distrib
-        subplot2(ny,numel(states)+2,5:6,1);hold('on');
-        ind = [stc{'lpause'}];
-        hax = bar(linspace(-pi/2,pi/2,50),histc(pch(ind,3),linspace(-pi/2,pi/2,50)),'histc');
-        hax.FaceColor = 'c';
-        hax.EdgeColor = 'c';
-        hax.FaceAlpha = 0.5;
-        hax.EdgeAlpha = 0.5;
-        ind = [stc{'hpause'}];
-        hax = bar(linspace(-pi/2,pi/2,50),histc(pch(ind,3),linspace(-pi/2,pi/2,50)),'histc');
-        hax.FaceColor = 'r';
-        hax.EdgeColor = 'r';
-        hax.FaceAlpha = 0.5;
-        hax.EdgeAlpha = 0.5;
-         
-        
-        
-        % row9-12col1 - ave spike waveform
-        subplot2(ny,numel(states)+2,[9:12],1);
-        hold('on');            
-        [~,sInd] = SelectPeriods(uRes,[stc{states{end},1}],'d',1,0);
-        if numel(sInd)>5,
-            mspk = bsxfun(@plus,sq(mean(uSpk(sInd,:,:)))',fliplr(1000:1000:8000));
-            plot(mspk,'b');
-            sspk = sq(std(uSpk(sInd,:,:)))';
-            plot(mspk-sspk,'r');
-            plot(mspk+sspk,'r');
-        end
-        xlim([0,52]);
-        ylim([0,9000]);
-
-        
-        
-        for s = 1:numel(states),
-
-% PLACEFIELDS 
-            subplot2(ny,numel(states)+2,[1,2],s+1);hold('on');
-            plot(pfkbs{s},unit,'mean',[],mpfsRate);
-            title(statesCcg{s});            
-            set(gca,'YTickLabel',{});set(gca,'XTickLabel',{});            
-            plot(mCom(s,u,2),mCom(s,u,1),'*m');
-            
-% TRANSITION Positions
-            subplot2(ny,numel(states)+2,[3,4],s+1);
-            hold('on');
-            plot(xyz(ceil(sper{s}{1}./1250.*xyz.sampleRate),7,1),...
-                 xyz(ceil(sper{s}{1}./1250.*xyz.sampleRate),7,2),'*g');
-            plot(xyz(ceil(sper{s}{2}./1250.*xyz.sampleRate),7,1),...
-                 xyz(ceil(sper{s}{2}./1250.*xyz.sampleRate),7,2),'*r');
-            xlim([-500,500]);ylim([-500,500]);
-            set(gca,'YTickLabel',{});set(gca,'XTickLabel',{});
-            
-% TRANITION triggered histogram onset
-            subplot2(ny,numel(states)+2,[5,6],s+1);
-            plot(bhvccg{s},unit,1);axis('tight');        title('onset');
-            ylim([0,mccgRate]);
-            
-% TRANITION triggered histogram offset
-            subplot2(ny,numel(states)+2,[7,8],s+1);
-            plot(bhvccg{s},unit,2);axis('tight');        title('offset');
-            ylim([0,mccgRate]);
-
-% WAVEFORM of unit
-            subplot2(ny,numel(states)+2,[9:12],s+1);
-            hold('on');            
-            [~,sInd] = SelectPeriods(uRes,[stc{states{s},1}],'d',1,0);
-            if numel(sInd)>5,
-                mspk = bsxfun(@plus,sq(mean(uSpk(sInd,:,:)))',fliplr(1000:1000:8000));
-                plot(bsxfun(@plus,mspk-mspkt,fliplr(250:250:2000)),'b');
-            end
-            xlim([0,52]);
-            ylim([0,2250]);
-            if s~=1,set(gca,'YTickLabel',{});set(gca,'XTickLabel',{});end
-            
-        end
-
-        for s = 1:numel(dfs),
-            subplot2(ny,numel(states)+2,[s*2:s*2+1]-1,numel(states)+2);
-            dfs{s}.plot(unit,'maxRate',mpfsRate,'isCircular',false);
-            hax = colorbar();
-            hax.Position(1) = hax.Position(1) + 0.05;
-            title(dfst{s});
-        end
-        
-        suptitle(['Trial: ',Trial.filebase,' Unit: ',num2str(unit)]);
-
-        
-        FigName = ['pfs','_',Trial.filebase,'_unit-',num2str(unit)];
-        %print(gcf,'-depsc2',fullfile(FigDir,[FigName,'.eps']));
-        print(gcf,'-dpng',  fullfile(FigDir,Trial.filebase,[FigName,'.png']));
-    end
-
-endo
+    % PLACEFIELD figure now located in placefield_summary.m
+end
